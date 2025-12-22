2f5536508ab3e086fb5c3ad4a6e920e8f89f18f4
Tiago Mota <tmota.seg@gmail.com>
2025-12-22 03:35:14 -0300

    Tipos Primitivos / integers

---
+++ src/tiposprimitivos/TiposPrimitivosNotasEstudos.java
@@ -0,0 +1,24 @@
+package tiposprimitivos;
+
+public class TiposPrimitivosNotasEstudos {
+    public static void main(String[] args) {
+        /*
+        // Caracteres || Characters
+        char primeiraLetraDoNome = 'T';
+        char primeiraLetraDoNomeASCII = 84;
+
+        System.out.println(primeiraLetraDoNome);
+        System.out.println(primeiraLetraDoNomeASCII);
+        */
+        // Inteiros || Integers
+        byte idade = 25; // -128 a 127
+        short ano = 2025; // -32768 a 32767
+        int salario = 10000; // -2147483648 a 2147483647
+        long populacao = 1000000L; // 19 dígitos
+
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(salario);
+        System.out.println(populacao);
+    }
+}
