17afc49577f01fdae35ad5b31d77a07390efd5ba
Tiago Mota <tmota.seg@gmail.com>
2025-12-22 01:57:22 -0300

    comentários / inserção de comentários com múltiplas linha e outros dois com apenas uma linha.

--- src/variaveis/VariaveisNotasEstudo.java
+++ src/variaveis/VariaveisNotasEstudo.java
@@ -1,11 +1,16 @@
+/* Indica que a classe abaixo 
+ * foi declarada no pacote [variaveis]
+ */
 package variaveis;
 
 public class VariaveisNotasEstudo {
     public static void main(String[] args) {
+        // Variáveis que representam a data atual.
         int dia = 22;
         int mes = 12;
         int ano = 2025;
 
+        // Impressão das variáveis.
         System.out.println(dia);
         System.out.println(mes);
         System.out.println(ano);
