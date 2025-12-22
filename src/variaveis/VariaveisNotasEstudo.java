85ff3b290a7d5004b61e5a23755de60580aae8c2
Tiago Mota <tmota.seg@gmail.com>
2025-12-22 02:05:21 -0300

    Variáveis / Atribuindo um novo valor à variável.

--- src/variaveis/VariaveisNotasEstudo.java
+++ src/variaveis/VariaveisNotasEstudo.java
@@ -1,14 +1,17 @@
 /* Indica que a classe abaixo 
- * foi declarada no pacote [variaveis]
+ * foi declarada no pacote [variaveis]:
  */
 package variaveis;
 
 public class VariaveisNotasEstudo {
     public static void main(String[] args) {
-        // Variáveis que representam a data atual.
+        // Variáveis que representam a data atual:
         int dia = 22;
         int mes = 12;
         int ano = 2025;
+
+        // Atribuindo um novo valor:
+        dia = 23;
 
         // Impressão das variáveis.
         System.out.println(dia);
