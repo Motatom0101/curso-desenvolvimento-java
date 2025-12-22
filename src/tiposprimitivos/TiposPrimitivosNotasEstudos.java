3933e9f795d91c5e645fb2934ca5a78479775e0c
Tiago Mota <tmota.seg@gmail.com>
2025-12-22 03:44:07 -0300

    Tipos Primitivos / Decimais e booleanos.

--- src/tiposprimitivos/TiposPrimitivosNotasEstudos.java
+++ src/tiposprimitivos/TiposPrimitivosNotasEstudos.java
@@ -9,7 +9,7 @@ public class TiposPrimitivosNotasEstudos {
 
         System.out.println(primeiraLetraDoNome);
         System.out.println(primeiraLetraDoNomeASCII);
-        */
+        
         // Inteiros || Integers
         byte idade = 25; // -128 a 127
         short ano = 2025; // -32768 a 32767
@@ -20,5 +20,18 @@ public class TiposPrimitivosNotasEstudos {
         System.out.println(ano);
         System.out.println(salario);
         System.out.println(populacao);
+        */
+        // Tipos Decimais:
+        float peso = 1.5f; // 6-7 dígitos significativos
+        double preco = 23.75; // 15 dígitos significativos
+        System.out.println(peso);
+        System.out.println(preco);
+
+        // Booleanos || Booleanos:
+        boolean verdadeiro = true;
+        boolean falso = false;
+        System.out.println(verdadeiro);
+        System.out.println(falso);
+
     }
 }
