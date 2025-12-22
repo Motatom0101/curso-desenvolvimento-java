e6fc2427c24de432b052cb47f83a3950998b4f83
Tiago Mota <tmota.seg@gmail.com>
2025-12-22 01:50:04 -0300

    variáveis inicializadas e usadas.

---
+++ src/variaveis/VariaveisNotasEstudo.java
@@ -0,0 +1,14 @@
+package variaveis;
+
+public class VariaveisNotasEstudo {
+    public static void main(String[] args) {
+        int dia = 22;
+        int mes = 12;
+        int ano = 2025;
+
+        System.out.println(dia);
+        System.out.println(mes);
+        System.out.println(ano);
+    
+    }
+}
