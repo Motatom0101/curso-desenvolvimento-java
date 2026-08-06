345be58e12c78aa8db6ccbb6f706ffa746e0cac3
Tiago Mota <tmota.seg@gmail.com>
2026-08-06 07:29:20 -0300

    POO/classe abstrata: Mostrando que da formaa que está, podemos instanciar uma classe abstrata, como foi o caso de 'SuperHeroi'.

--- principios-poo/src/heranca/Main.java
+++ principios-poo/src/heranca/Main.java
@@ -7,7 +7,8 @@ public class Main {
                 new HomemAranha("Roupa Vermelha com teias.", new String[]{"Lançar Teia."}),
                 new HomemDeFerro("Roupa Vermelha super resistente.", new String[]{"Voar."}),
                 new Tempestade("Branca.", new String[]{"Controlar o tempo."}),
-                new ViuvaNegra("Preta.", new String[]{"Lançar Teia."})
+                new ViuvaNegra("Preta.", new String[]{"Lançar Teia."}),
+                new SuperHeroi("Traje invisível", new String[]{"Bênção e maldição."})
         };
 
         validar(superHerois);
