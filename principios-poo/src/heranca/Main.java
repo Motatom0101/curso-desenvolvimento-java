7bd0e79c8f49e13fe067ce36c75201923079b16b
Tiago Mota <tmota.seg@gmail.com>
2026-08-06 07:24:49 -0300

    POO/instanceof: retornando a comparação do operador binário para a classe concreta 'HomemAranha'.

--- principios-poo/src/heranca/Main.java
+++ principios-poo/src/heranca/Main.java
@@ -20,7 +20,7 @@ public class Main {
     }
 
     static void validar(SuperHeroi[] superHerois) {
-        if (superHerois[0] instanceof Object) {
+        if (superHerois[0] instanceof HomemAranha) {
             System.out.println("Homem Aranha validado!");
 
         }
